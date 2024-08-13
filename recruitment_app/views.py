9b42b1eebf18104b43a4ac70f632b7f925896fa3
KCCI119\kcci <rundollyrun8@gmail.com>
2024-08-13 18:32:44 +0900

    [feat] user create, resume create & read(list) 구현

--- recruitment_app/views.py
+++ recruitment_app/views.py
@@ -5,20 +5,62 @@ from rest_framework.generics import CreateAPIView
 from rest_framework import viewsets
 from django.db.models import Q
 from .models import Company, User, Recruitment, Resume
-from .serializer import RecruitmentListSerializer, RecruitmentCreatedSerializer, RecruitmentDetailSerializer, RecruitmentUpdatedSerializer, CompanySerializer
 
+from .serializers.company import ( CompanySerializer,)
+from .serializers.user import (UserSerializer,)
+from .serializers.resume import (ResumeSerializer,)
+from .serializers.recruitment import (
+    RecruitmentListSerializer,
+    RecruitmentCreatedSerializer,
+    RecruitmentDetailSerializer,
+    RecruitmentUpdatedSerializer,
+)
+
+# company ViewSet
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
 
+    # create new company
     def create(self, request):
         serializer = CompanySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
-    
 
+# user ViewSet
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    # create new user
+    def create(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            self.perform_create(serializer)
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+# resume ViewSet
+class ResumeViewSet(viewsets.ModelViewSet):
+    queryset = Resume.objects.all()
+    serializer_class = ResumeSerializer
+
+    # create new resume
+    def create(self, request, *args, **kwarges):
+        serializer = ResumeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+    
+    def list(self, request):
+        queryset = Resume.objects.all()
+        serializer = ResumeSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+# 채용 공고 ViewSet
 class RecruitmentViewSet(viewsets.ModelViewSet):
     queryset = Recruitment.objects.all()
     
@@ -33,31 +75,33 @@ class RecruitmentViewSet(viewsets.ModelViewSet):
             return RecruitmentListSerializer
         return super().get_serializer_class()
     
+    # 공고 목록 조회(검색 가능 => query string : search)
     def list(self, request):
+        # get search value
+        search_query = request.query_params.get('search', None)
         queryset = Recruitment.objects.all()
+
+        if search_query:
+            # if search_query can change int type, change type and input to search_compensation
+            try:
+                search_compensation = int(search_query)
+            except ValueError:
+                search_compensation = None
+
+            queryset = queryset.filter(
+                # icontans => case-free
+                Q(company__company_name__icontains=search_query) |
+                Q(company__country__icontains=search_query) |
+                Q(company__region__icontains=search_query) |
+                Q(position__icontains=search_query) |
+                # search_compensation is not None, can search
+                (Q(compensation=search_compensation) if search_compensation is not None else Q()) |
+                Q(skill__icontains=search_query)
+            )
         serializer = RecruitmentListSerializer(queryset, many=True)
         return Response(serializer.data)
     
-    def filter_queryset(self, queryset):
-        search_params = {
-            'company_name': self.request.query_params.get('company_name', None),
-            'country': self.request.query_params.get('country', None),
-            'region': self.request.query_params.get('region', None),
-            'position': self.request.query_params.get('position', None),
-            'compensation': self.request.query_params.get('compensation', None),
-            'skill': self.request.query_params.get('skill', None)
-        }
-
-        filters = Q()
-        for key, value in search_params.items():
-            if value:
-                if key in ['compensation']:
-                    filters &= Q(**{f'{key}__exact': value})
-                else:
-                    filters &= Q(**{f'company__{key}__icontains': value}) | Q(**{f'{key}__icontains': value})
-
-        return queryset.filter(filters)
-    
+    # 공고 신규 등록
     def create(self, request):
         serializer = RecruitmentCreatedSerializer(data=request.data)
         if serializer.is_valid():
@@ -65,8 +109,8 @@ class RecruitmentViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
     
+    # 단 건 조회
     def retrieve(self, request, pk=None):
-
         try:
             recruitment = Recruitment.objects.get(pk=pk)
         except Recruitment.DoesNotExist:
@@ -74,6 +118,7 @@ class RecruitmentViewSet(viewsets.ModelViewSet):
         serializer = RecruitmentDetailSerializer(recruitment)
         return Response(serializer.data)
 
+    # 수정
     def update(self, request, pk=None):
         try:
             recruitment = Recruitment.objects.get(pk=pk)
@@ -85,6 +130,7 @@ class RecruitmentViewSet(viewsets.ModelViewSet):
             return Response(serializer.data)
         return Response(serializer.errors, status=400)
 
+    # 삭제
     def destroy(self, request, pk=None):
         try:
             recruitment = Recruitment.objects.get(pk=pk)
