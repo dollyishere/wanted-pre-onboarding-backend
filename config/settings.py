708b8e8891a226c21cec80d29dfde567e2378ab0
KCCI119\kcci <rundollyrun8@gmail.com>
2024-08-13 18:34:16 +0900

    [refactor] settings.py 일부 수정

--- config/settings.py
+++ config/settings.py
@@ -98,10 +98,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # connect sqlite3
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+
+    # connect postgreSQL
     'default' : {
         'ENGINE' : 'django.db.backends.postgresql',
         'NAME' : env("DB_NAME"),
